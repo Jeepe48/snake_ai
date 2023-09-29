f467b1c9650e852b918e9a2e536ac57c0e7e2b66
Аlexey <muholov2004@gmail.com>
2023-09-30 01:36:57 +0200

    fix bugs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 
 winSize = (600, 600)
-fps = 10
+fps = 5
 
 apple_size = 10
 snake_size = 20
@@ -43,47 +43,48 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT and direction != 2:
+                direction = 0
+            elif event.key == pygame.K_DOWN and direction != 3:
+                direction = 1
+            elif event.key == pygame.K_LEFT and direction != 0:
+                direction = 2
+            elif event.key == pygame.K_UP and direction != 1:
+                direction = 3
+            elif event.key == pygame.K_SPACE and not alive:
+                alive = True
+                snake = [start_pos]
+                apple = random.randint(0, borders[0] - 1), random.randint(0, borders[1] - 1)
+                fps = 5
+
 
     for x, y in snake:
-        pygame.draw.rect(screen, green, (x * snake_size, y * snake_size, snake_size - 1, snake_size - 1))
-    pygame.draw.rect(screen, red, (apple[0] * snake_size, apple[1] * snake_size, snake_size - 1, snake_size - 1))
+        # Проверка, является ли текущая позиция головой
+        if (x, y) == snake[0]:
+            # Рисование головы красного цвета
+             pygame.draw.rect(screen, (0, 128, 0), (x * snake_size, y * snake_size, snake_size - 1, snake_size - 1))
+        else:
+            # Рисование остальных сегментов зеленым цветом
+            pygame.draw.rect(screen, green, (x * snake_size, y * snake_size, snake_size - 1, snake_size - 1))
+        pygame.draw.rect(screen, red, (apple[0] * snake_size, apple[1] * snake_size, snake_size - 1, snake_size - 1))
 
     new_pos = (
         (snake[0][0] + directions[direction][0]) % borders[0],  # * speedSnake,
         (snake[0][1] + directions[direction][1]) % borders[1] # * speedSnake,
     )
 
-    # Обработка событий клавиш
-    keys = pygame.key.get_pressed()
     if alive:
-        if keys[pygame.K_RIGHT] and direction !=2:
-            direction = 0
-        if keys[pygame.K_DOWN] and direction !=3:
-            direction = 1
-        if keys[pygame.K_LEFT] and direction !=0:
-            direction = 2
-        if keys[pygame.K_UP] and direction !=1:
-            direction = 3
-    else:
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]:
-            alive = True
-            snake = [start_pos]
-            apple = random.randint(0, borders[0] - 1), random.randint(0, borders[1] - 1)
-            fps = 5
-
-    if alive:
-        if new_pos in tail:
-            alive = False
+        snake.insert(0, new_pos)
+        if new_pos == apple:
+            fps += 1 and not fps >= 15
+            apple = random.randint(0, borders[0]-1), random.randint(0, borders[1]-1)
         else:
-            snake.insert(0, new_pos)
-            if new_pos == apple:
-                fps += 1
-                apple = random.randint(0, borders[0]-1), random.randint(0, borders[1]-1)
-            else:
-                tail = snake[1:]
-                snake.pop(-1)
-    else:
+            snake.pop(-1)
+        tail = snake[1:]
+
+    if new_pos in tail:
+        alive = False
         text = font_gameover.render(f"GAME OVER", True, "white")
         screen.blit(text, (winSize[0] // 2 - text.get_width() // 2, winSize[1] // 2 - 50))
         text = font_space.render(f"Press SPACE for restart", True, "white")
